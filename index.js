7bb21fcfdb64e1fe9cb51013f8a07d32e6a1c4d6
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-28 08:28:20 -0700

    Upgrade repository for compatability

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var RESOLVING = 1;
 var RESOLVED_SUCCESS = 2;
 var RESOLVED_ERROR = 3;
 
-module.exports = function(fn) {
+module.exports = function (fn) {
   var state = UNRESOLVED;
   var result;
   var waiting = [];
@@ -13,23 +13,23 @@ module.exports = function(fn) {
     state = RESOLVING;
 
     Promise.resolve(fn())
-      .then(function(value) {
+      .then(function (value) {
         state = RESOLVED_SUCCESS;
         result = value;
         while (waiting.length) waiting.pop().resolve(result);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         state = RESOLVED_ERROR;
         result = err;
         while (waiting.length) waiting.pop().reject(result);
       });
   }
 
-  return function() {
+  return function () {
     if (state === RESOLVED_SUCCESS) return Promise.resolve(result);
     else if (state === RESOLVED_ERROR) return Promise.reject(result);
 
-    const promise = new Promise(function(resolve, reject) {
+    const promise = new Promise(function (resolve, reject) {
       waiting.push({ resolve: resolve, reject: reject });
     });
     resolveResult();
